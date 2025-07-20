1c16a51c8b38f145e787da7c22b2d92ebfdf8c75
Yana Malakhova <ya6ka0@gmail.com>
2025-07-21 01:49:44 +0300

    test: add ErrorBoundary tests

---
+++ rs-react-app/tests/components/ErrorBoundary.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import ErrorBoundary from '../../src/app/components/ErrorBoundary/ErrorBoundary';
+
+describe('ErrorBoundary', () => {
+  it('should render content when no error', () => {
+    render(
+      <ErrorBoundary>
+        <div>Some content</div>
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText('Some content')).toBeInTheDocument();
+    expect(screen.queryByText(/something went wrong/i)).not.toBeInTheDocument();
+  });
+
+  it('should show error message, when child throws error', () => {
+    vi.spyOn(console, 'error').mockImplementation(() => {});
+
+    const ErrorComponent = () => {
+      throw new Error('ErrorBoundary test');
+    };
+
+    render(
+      <ErrorBoundary>
+        <ErrorComponent />
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText(/something went wrong/i)).toBeInTheDocument();
+    expect(screen.getByText(/Please Try Again/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Try Again/i })
+    ).toBeInTheDocument();
+
+    vi.restoreAllMocks();
+  });
+});
