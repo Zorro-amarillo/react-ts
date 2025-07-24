b466181fd4013f71bda0dbd4a8b0efd97d0f9f15
Yana Malakhova <ya6ka0@gmail.com>
2025-07-24 13:52:27 +0300

    refactor: rewrite App class component to functional

--- rs-react-app/src/app/App.tsx
+++ rs-react-app/src/app/App.tsx
@@ -1,18 +1,15 @@
 import './App.css';
-import { Component } from 'react';
 import SearchPanel from './components/SearchPanel/SearchPanel';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
-class App extends Component {
-  render() {
-    return (
-      <ErrorBoundary>
-        <div className="app">
-          <SearchPanel />
-        </div>
-      </ErrorBoundary>
-    );
-  }
-}
+const App = () => {
+  return (
+    <ErrorBoundary>
+      <div className="app">
+        <SearchPanel />
+      </div>
+    </ErrorBoundary>
+  );
+};
 
 export default App;
