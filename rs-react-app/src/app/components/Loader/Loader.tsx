6515c0abc7ec87446d4e073bb036121bf82967da
Yana Malakhova <ya6ka0@gmail.com>
2025-07-24 14:17:33 +0300

    refactor: rewrite Loader class component to functional

--- rs-react-app/src/app/components/Loader/Loader.tsx
+++ rs-react-app/src/app/components/Loader/Loader.tsx
@@ -1,18 +1,15 @@
 import loader from '../../../assets/loader.gif';
-import { Component } from 'react';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className="w-full flex justify-center">
-        <img
-          src={loader}
-          alt="Loading"
-          className="w-auto max-w-64 max-h-64 mx-auto"
-        />
-      </div>
-    );
-  }
-}
+const Loader = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <img
+        src={loader}
+        alt="Loading"
+        className="w-auto max-w-64 max-h-64 mx-auto"
+      />
+    </div>
+  );
+};
 
 export default Loader;
