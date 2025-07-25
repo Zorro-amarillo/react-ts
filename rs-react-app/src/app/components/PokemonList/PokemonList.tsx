c39a27190fb8e7e74a8916481e98d886d3a1d3fd
Yana Malakhova <ya6ka0@gmail.com>
2025-07-26 00:46:45 +0300

    refactor: rewrite PokemonList class component to functional

--- rs-react-app/src/app/components/PokemonList/PokemonList.tsx
+++ rs-react-app/src/app/components/PokemonList/PokemonList.tsx
@@ -1,39 +1,33 @@
 import './PokemonList.css';
-import { Component } from 'react';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import type { IPokemonListProps } from '../../../types/types';
 import Pokemon from '../Pokemon/Pokemon';
-import PokemonService from '../../services/PokemonService/PokemonService';
 
-class PokemonList extends Component<IPokemonListProps> {
-  pokemonService = new PokemonService();
+const PokemonList = (props: IPokemonListProps) => {
+  const { data } = props;
 
-  render() {
-    const { data } = this.props;
-
-    const pokemonElements = data.map((pokemon, index) => {
-      if (!pokemon) {
-        return (
-          <ErrorBoundary key={`error-boundary-${index}`}>
-            <h3 className="mb-3 mt-4 text-gray-500 text-2xl font-bold">
-              ⚠️ Invalid Pokemon Data ⚠️
-            </h3>
-            <p className="mb-4 mt-3 text-gray-500 text-xl font-bold">
-              Please Enter Full Name or Id
-            </p>
-          </ErrorBoundary>
-        );
-      }
-
+  const pokemonElements = data.map((pokemon, index) => {
+    if (!pokemon) {
       return (
         <ErrorBoundary key={`error-boundary-${index}`}>
-          <Pokemon key={index} pokemonData={pokemon} />
+          <h3 className="mb-3 mt-4 text-gray-500 text-2xl font-bold">
+            ⚠️ Invalid Pokemon Data ⚠️
+          </h3>
+          <p className="mb-4 mt-3 text-gray-500 text-xl font-bold">
+            Please Enter Full Name or Id
+          </p>
         </ErrorBoundary>
       );
-    });
+    }
 
-    return <ul className="search-results">{pokemonElements}</ul>;
-  }
-}
+    return (
+      <ErrorBoundary key={`error-boundary-${index}`}>
+        <Pokemon key={index} pokemonData={pokemon} />
+      </ErrorBoundary>
+    );
+  });
+
+  return <ul className="search-results">{pokemonElements}</ul>;
+};
 
 export default PokemonList;
