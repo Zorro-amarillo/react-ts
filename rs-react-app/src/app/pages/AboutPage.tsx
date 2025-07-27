5529fea6f324ccbac8da8f44707902cd6a4dae40
Yana Malakhova <ya6ka0@gmail.com>
2025-07-27 15:53:06 +0300

    feat: improve Page404 layout

--- rs-react-app/src/app/pages/AboutPage.tsx
+++ rs-react-app/src/app/pages/AboutPage.tsx
@@ -1,5 +1,5 @@
 import Footer from '../components/Footer/Footer';
-import { Link } from 'react-router-dom';
+import BackToMainButton from '../components/BackToMainButton/BackToMainButton';
 
 const AboutPage = () => {
   return (
@@ -31,12 +31,7 @@ const AboutPage = () => {
           </a>{' '}
           to master modern frontend development.
         </p>
-        <Link
-          to="/"
-          className="bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          <span className="text-white">Back to Main Page</span>
-        </Link>
+        <BackToMainButton />
       </main>
       <Footer />
     </>
--- rs-react-app/src/app/pages/Page404.tsx
+++ rs-react-app/src/app/pages/Page404.tsx
@@ -1,12 +1,12 @@
 import page404 from '../../assets/page404.png';
-import { Link } from 'react-router-dom';
+import BackToMainButton from '../components/BackToMainButton/BackToMainButton';
 
 const Page404 = () => {
   return (
-    <>
-      <img src={page404} alt="" className="mb-5 max-w-[500px]" />
-      <Link to="/">Back to Main Page</Link>
-    </>
+    <main className="p-8 flex-grow flex-col flex items-center justify-center">
+      <img src={page404} alt="" className="mb-5 max-w-[500px] mx-auto" />
+      <BackToMainButton />
+    </main>
   );
 };
 
