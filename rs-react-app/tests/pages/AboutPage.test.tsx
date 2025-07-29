fedebbcd7dd01e9f5bf9147e5b9e553449a8024f
Yana Malakhova <ya6ka0@gmail.com>
2025-07-29 12:24:40 +0300

    test: add AboutPage tests

---
+++ rs-react-app/tests/pages/AboutPage.test.tsx
@@ -0,0 +1,24 @@
+import AboutPage from '../../src/app/pages/AboutPage';
+import { screen } from '@testing-library/react';
+import { renderWithRouter } from '../test-utils/test-utils';
+
+describe('AboutPage', () => {
+  it('should render content correctly', () => {
+    renderWithRouter(<AboutPage />);
+
+    const heading = screen.getByRole('heading', {
+      name: /about the developer/i,
+    });
+    expect(heading).toBeInTheDocument();
+    expect(screen.getByText(/Yana Malakhova/i)).toBeInTheDocument();
+  });
+
+  it('should contain RSSchool React course link', () => {
+    renderWithRouter(<AboutPage />);
+
+    const link = screen.getByRole('link', {
+      name: /RSSchool React course/i,
+    });
+    expect(link).toHaveAttribute('href', 'https://rs.school/courses/reactjs');
+  });
+});
