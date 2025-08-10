10af432fb0f8b23c296df1bcc49658f77fe102a7
Yana Malakhova <ya6ka0@gmail.com>
2025-08-10 20:08:50 +0300

    refactor: remove duplicate isLoading check in PokemonDetails

--- src/components/pokemons/PokemonDetails.tsx
+++ src/components/pokemons/PokemonDetails.tsx
@@ -38,19 +38,17 @@ const PokemonDetails = () => {
   return (
     <>
       <BackToMainButton text="Close Card" />
-      {isLoading ? (
-        <div className="mt-10">
-          <Loader />
-        </div>
-      ) : (
-        <>
-          <h3 className="text-gray-900 mt-10">{`${pokemonName[0].toUpperCase()}${pokemonName?.slice(1)}`}</h3>
-          <img src={pokemonData.sprites.front_default} alt="Pokemon Image" className="mx-auto" />
-          <p className="text-gray-500">{`Height: ${pokemonData.height}`}</p>
-          <p className="text-gray-500">{`Weight: ${pokemonData.weight}`}</p>
-        </>
-      )}
-      <button className={`${btnPrimaryStyle} mt-8`} onClick={refetch}>
+      <h3 className="text-gray-900 mt-10">{`${pokemonName[0].toUpperCase()}${pokemonName?.slice(1)}`}</h3>
+      <img src={pokemonData.sprites.front_default} alt="Pokemon Image" className="mx-auto" />
+      <p className="text-gray-500">{`Height: ${pokemonData.height}`}</p>
+      <p className="text-gray-500">{`Weight: ${pokemonData.weight}`}</p>
+      <button
+        className={`${btnPrimaryStyle} mt-8`}
+        onClick={() => {
+          refetch();
+          console.info(`Cache for ${pokemonName} is cleared`);
+        }}
+      >
         Clear Pokemon Cache
       </button>
     </>
